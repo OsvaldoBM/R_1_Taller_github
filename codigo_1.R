42e82a2a01929a05c7a4824fb8865c2c95b00430
OsvaldoBM <86447391+OsvaldoBM@users.noreply.github.com>
2022-08-27 09:24:01 -0500

    Update README.md

---
+++ codigo_1.R
@@ -0,0 +1,17 @@
+###############################################################################
+#                 TALLer de Github con R                                    #
+###############################################################################
+
+
+# Introducción a Github
+#1. instalar  Git
+#2. Crear repositorio de Github
+#3. Crear un proyecto con versi[on control *Git()]
+#4. Clonar repositorio a la computadora.
+
+#Crear c[odigo]
+df<-mtcars
+names(mtcars)
+
+summary(mtcars)
+plot(df$hp,main = "Gráfico")
